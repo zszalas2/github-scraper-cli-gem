5a53ef4a71bb91b2d901f1d73d10cb4c118063c8
zszalas2 <zszalas@gmail.com>
2017-01-15 16:14:24 -0500

    Possibly found name scrape solution

--- lib/github_scraper.rb
+++ lib/github_scraper.rb
@@ -11,6 +11,21 @@ class GithubScraper
     @doc = Nokogiri::HTML(open("https://github.com/search?utf8=%E2%9C%93&q=#{query}&type=Users&ref=searchresults"))
   end
 
+  def scrape
+
+    @search_result #=> this should return everything related to search_result
+  end
+
+  def scrape_details #populate search_result w/ name, email, url, location, username
+    name =  
+    user_name = @doc.css("div .user-list-info a").children.text
+    email_address @doc.css("#user_search_results a.email").each do |em| puts em.text end
+    location = @doc.css("div.user-list-info li")[0].text
+    name = @doc.css("div.user-list-info").children[4].text.rstrip
+
+  end
+
+
   #retrieves all the data needed to retrieve objects
   #this would have to make  search_result class
 
